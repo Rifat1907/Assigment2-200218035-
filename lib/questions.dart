2d753fe54d27fa7d1403cf1b9326a0b2d33626b3
Rifat Tekşam <110569574+Rifat1907@users.noreply.github.com>
2023-04-07 16:39:52 +0300

    Initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,32 @@
+final List<Map<String, dynamic>> questions = [
+  {
+    'question': 'In which continent is brasil',
+    'options': ['Afrika', 'Asia', 'Europe', 'North America', 'South America'],
+    'Answer': 'South America',
+  },
+  {
+    'question': 'Whic footbal team is Spanish?',
+    'options': ['Manchester City', 'FENERBAHÇE', 'Ajax', 'Real Madrid'],
+    'answer': 'Real Madrid',
+  },
+  {
+    'question': 'Which car is italian?',
+    'options': ['Ferrari', 'Volkswagen', 'Mercedes', 'Toyota'],
+    'answer': 'Ferarri',
+  },
+  {
+    'question': 'Who discovered America',
+    'options': [
+      'Francesco Totti',
+      'Cristoph Colomb',
+      'Albert Einstein',
+      'Shakira'
+    ],
+    'answer': 'Cristoph Colomb',
+  },
+  {
+    'question': 'How many cities does turkey have',
+    'options': ['52', '81', '83', '66'],
+    'answer': '81',
+  },
+];
